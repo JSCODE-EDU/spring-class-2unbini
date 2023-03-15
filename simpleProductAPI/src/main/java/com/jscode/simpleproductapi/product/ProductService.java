92e4dea7f08b58b66ccf690d45fa7712e348f920
Eunbin Kwon <kwoneunbini@gmail.com>
2023-03-16 01:01:45 +0900

    [feat] Simple Product API 생성(GET, POST)

---
+++ simpleProductAPI/src/main/java/com/jscode/simpleproductapi/product/ProductService.java
@@ -0,0 +1,54 @@
+package com.jscode.simpleproductapi.product;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    final ProductRepository productRepository;
+
+    ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public Product getProductInformationWithID(int id) throws Exception {
+        Product product = productRepository.getProductInformationWithID(id);
+
+        if (product == null) {
+            throw new NullPointerException();
+        }
+
+        return product;
+    }
+
+    public Product getProductInformationWithName(String name) throws Exception {
+        Product product = productRepository.getProductInformationWithName(name);
+
+        if (product == null) {
+            throw new NullPointerException();
+        }
+
+        return product;
+    }
+
+    public Product postNewProduct(Product newProduct) throws Exception {
+        Product postedProduct = productRepository.postNewProduct(newProduct);
+
+        if (postedProduct == null) {
+            throw new NullPointerException();
+        }
+
+        return postedProduct;
+    }
+
+    public String generateReturnString(Product product) {
+        if (product.isOnSale) {
+            return String.format("product: %s(%d) is now on sale. cost: %d won.", product.name, product.id, product.cost);
+        } else {
+            return String.format("product: %s(%d) is now out of stock. cost: %d won.", product.name, product.id, product.cost);
+        }
+    }
+
+    public String generateReturnStringWhenPosted(Product product) {
+        return String.format("product No. %d, %s. Now on sale for %d won.", product.id, product.name, product.cost);
+    }
+}
---
+++ simpleProductAPI/src/test/java/com/jscode/simpleproductapi/SimpleProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jscode.simpleproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProductApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
