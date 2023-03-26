3a620cc79656b82e74abbbbdd53684fa8e9562bd
Eunbin Kwon <kwoneunbini@gmail.com>
2023-03-26 13:50:31 +0900

    [feat] JPA 레포지토리 이용해 리팩토링

--- simpleProductAPI/src/main/java/com/jscode/simpleproductapi/service/ProductService.java
+++ simpleProductAPI/src/main/java/com/jscode/simpleproductapi/service/ProductService.java
@@ -1,56 +1,40 @@
 package com.jscode.simpleproductapi.service;
+import com.jscode.simpleproductapi.entity.Product;
 
-import com.jscode.simpleproductapi.dto.Product;
-import com.jscode.simpleproductapi.repository.ProductRepository;
+import com.jscode.simpleproductapi.repository.ProductJPARepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
+
 public class ProductService {
+    private final ProductJPARepository productJPARepository;
 
-    final ProductRepository productRepository;
-
-    ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public List<Product> findAll() {
+        return productJPARepository.findAll();
     }
-    public Product getProductInformationWithID(int id) throws Exception {
-        Product product = productRepository.getProductInformationWithID(id);
+
+    public Product getProductInformationWithID(Long id) throws Exception {
+        Optional<Product> product = productJPARepository.findById(id);
 
         if (product == null) {
             throw new NullPointerException();
         }
 
-        return product;
-    }
-
-    public Product getProductInformationWithName(String name) throws Exception {
-        Product product = productRepository.getProductInformationWithName(name);
-
-        if (product == null) {
-            throw new NullPointerException();
-        }
-
-        return product;
+        return product.get();
     }
 
     public Product postNewProduct(Product newProduct) throws Exception {
-        Product postedProduct = productRepository.postNewProduct(newProduct);
+        Product postedProduct = productJPARepository.save(newProduct);
 
         if (postedProduct == null) {
             throw new NullPointerException();
         }
 
         return postedProduct;
-    }
-
-    public String generateReturnString(Product product) {
-        if (product.isOnSale) {
-            return String.format("product: %s(%d) is now on sale. cost: %d won.", product.name, product.id, product.cost);
-        } else {
-            return String.format("product: %s(%d) is now out of stock. cost: %d won.", product.name, product.id, product.cost);
-        }
-    }
-
-    public String generateReturnStringWhenPosted(Product product) {
-        return String.format("product No. %d, %s. Now on sale for %d won.", product.id, product.name, product.cost);
     }
 }
